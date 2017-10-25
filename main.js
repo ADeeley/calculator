07ee46390746ab97054e111c0d73fbd6c2078ef2
Adam M Deeley <ad.deeley@gmail.com>
2017-10-25 12:32:47 +0100

    Added expression to string function for parsing the input.

--- main.js
+++ main.js
@@ -1,7 +1,64 @@
+function Operators() {
+    var symbols = {
+        "-" : 0,
+        "+" : 1,
+        "*" : 2, 
+        "/" : 3
+    }
+
+    this.isOperator = function(val) {
+        /**
+         * Returns true if argument is an operator.
+         */
+        if (!symbols[val]) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    this.getPriority = function(val) {
+        /** 
+         * Returns the priority of the argument as an int.
+         */
+        return symbols[val];
+    }
+}
+
+function exprStrToArr(str) {
+    /**
+     * returns an array of numbers and operators.
+     */
+    var expressionArr = [];
+    var symbol = "";
+    for (var i = 0; i < str.length; i++) {
+        switch (str[i]) {
+            case "/":
+            case "*":
+            case "+":
+            case "-":
+                expressionArr.push(symbol);
+                symbol = "";
+                expressionArr.push(str[i]);
+                break;
+            default:
+                symbol += str[i];
+                break;
+        }
+    }
+    // Push the last symbol to the array
+    expressionArr.push(symbol);
+
+    return expressionArr;
+}
+
 window.onload = function() {
-    console.log("Test");        
+    var ops = new Operators();
+    console.log(exprStrToArr("12*4+8-89/34"));
     document.getElementById("buttons").addEventListener("click", detectButton);
 }
+
 
 function updateExpression(val) {
     /**
@@ -11,18 +68,19 @@ function updateExpression(val) {
     document.getElementById("screen").value += val; 
 }
 
-function calculate() {
+function calculate(expr) {
     /**
      * Calculates the expression and returns the result to the input
      * screen.
      */
-    console.log("Calculate called");
+
 }
 
 function clear() {
     /**
      * Clears the whole input.
      */
+    document.getElementById("screen").value = "";
     console.log("Clear called");
 }
 
@@ -61,7 +119,7 @@ function detectButton() {
                 console.log("Test");
                 break;
             case "=":
-                calculate();
+                calculate(document.getElementById("screen").value);
                 break;
             case "C":
                 clear();
