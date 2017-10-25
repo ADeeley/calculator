217b797ba0927df29a23050eb8c7b18c9dbfb5c0
Adam M Deeley <ad.deeley@gmail.com>
2017-10-25 09:54:37 +0100

    Added a filter for the input events - now skips over the div container.

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 window.onload = function() {
     console.log("Test");        
+    document.getElementById("buttons").addEventListener("click", updateExpression);
 }
 
+function updateExpression() {
+    console.log("Update Expr");
+    var input = event.target;
+    console.log(input.tagName);
+    if (input.tagName == "BUTTON") {
+        document.getElementById("screen").value += event.target.innerHTML; 
+    }
+};
 
